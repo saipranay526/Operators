37f98e5f8d0720f867248f3cd7bb377adff31247
saipranay526 <vardellisaipranay@gmail.com>
2025-01-28 19:47:30 +0530

    Add files via upload

---
+++ Opr.java
@@ -0,0 +1,25 @@
+public class Opr {
+    public static void main (String[]args){
+        int a = 40;
+        int b = 50;
+//ARTHEMATIC OPERATIONS ON INTEGER DATA TYPE
+        System.out.println(a); //println directly enter the next code in new line atomatically 
+        System.out.printf("the value of a is %d \n",a);// new line (escape char) should be mentioned ,to get the code in new line)
+        System.out.println(b);
+        System.out.printf("the value of b is %d \n",b);
+        System.out.println(a+b);
+        System.out.printf("the sum of %d + %d is %d \n",a,b,a+b);
+        System.out.println(a-b);
+        System.out.printf("the sum of %d  - %d is %d \n",a,b,a-b);
+        System.out.println(a*b);
+        System.out.printf("the sum of %d  mlt by %d is %d \n",a,b,a*b);
+        System.out.println(a/b);
+        System.out.printf("the sum of %d div %d is %d \n ",a,b,a/b);
+        System.out.println(a%b);
+        System.out.printf("the sum of %d mod %d is %d \n ",a,b,a%b);
+//left shift and right shift operators
+        System.out.println(125<<1);
+        System.out.println(5>>1);
+    }
+    
+}
